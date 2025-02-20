e8980cb66cb7c6e058f19c705a39e6256ff5c976
Oleg Stobbe <oleg@bitforr.com>
2025-02-20 20:15:00 +0100

    fix: fix handling of initial push events

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
+/** The before commit SHA used in the initial push event */
+export const INITIAL_PUSH_BEFORE_COMMIT_SHA: string
+    = '0000000000000000000000000000000000000000';
+
 /** Action's output name */
 export enum OutputName {
   COMMIT_RANGE = 'commit-range',
